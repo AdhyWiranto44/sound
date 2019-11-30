ef575318cfbc37c97f40e4b6fc9745a406947298
lidyaislamiatiz <lidyaislamiati04@gmail.com>
2019-11-30 19:51:54 +0700

    mengerjakan video #5

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,18 +1,18 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class User extends CI_Controller
+class Admin extends CI_Controller
 {
     public function index()
     {
-        $data['title'] = 'My Profile';
+        $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
 
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
-        $this->load->view('user/index', $data);
+        $this->load->view('admin/index', $data);
         $this->load->view('templates/footer');
     }
 }
