785c962c5cd58b4c82477e5c58a9bfc732ea59e4
AdhyWiranto44 <adhywiranto68@gmail.com>
2019-12-03 18:02:38 +0700

    mengerjakan video #6

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -4,58 +4,85 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('user'); ?>">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fab fa-angellist"></i>
+            <i class="fas fa-code"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Rekweb Admin</div>
+        <div class="sidebar-brand-text mx-3">Tubes RekWeb</div>
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Admin
-    </div>
+    <!-- QUERY -->
+    <?php
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                  FROM `user_access_menu` JOIN `user_menu`
+                    ON `user_access_menu`.`menu_id` = `user_menu`.`id`
+                 WHERE `user_access_menu`.`role_id` = $role_id
+              ORDER BY `user_access_menu`.`menu_id` ASC
+                ";
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('admin/index'); ?>">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+    $menu = $this->db->query($queryMenu)->result_array();
+    ?>
+    <!-- end of QUERY -->
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+    <!-- prepare menu -->
+    <?php foreach ($menu as $m) : ?>
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            <?= $m['menu']; ?>
+        </div>
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        USer
-    </div>
+        <?php
+            $menuId = $m['id'];
+            $querySubMenu = "SELECT *
+                         FROM `user_sub_menu`
+                        WHERE `user_sub_menu`.`menu_id` = $menuId
+                          AND `is_active` = 1
+                    ";
 
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('user/index'); ?>">
-            <i class="fas fa-fw fa-user"></i>
-            <span>My Profile</span></a>
-    </li>
+            $subMenu = $this->db->query($querySubMenu)->result_array();
+            ?>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+        <!-- prepare sub menu -->
+        <?php foreach ($subMenu as $sm) : ?>
+            <!-- Nav Item -->
+            <?php if ($title == $sm['title']) : ?>
+                <li class="nav-item active">
+                <?php else : ?>
+                <li class="nav-item">
+                <?php endif; ?>
+                <a class="nav-link pb-0" href="<?= $sm['url']; ?>">
+                    <i class="<?= $sm['icon']; ?>"></i>
+                    <span><?= $sm['title']; ?></span></a>
+                </li>
+            <?php endforeach; ?>
 
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('auth/logout'); ?>">
-            <i class="fas fa-sign-out-alt"></i>
-            <span>Logout</span></a>
-    </li>
+            <!-- Divider -->
+            <hr class="sidebar-divider mt-3">
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+        <?php endforeach; ?>
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+
+
+
+        <!-- Divider -->
+        <!-- <hr class="sidebar-divider d-none d-md-block"> -->
+
+        <!-- Nav Item -->
+        <li class="nav-item">
+            <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
+                <i class="fas fa-fw fa-sign-out-alt"></i>
+                <span>Logout</span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
 
 </ul>
 <!-- End of Sidebar -->
