c888172b9ca091adc79bb68866ff07fa26e288ab
AdhyWiranto44 <adhywiranto68@gmail.com>
2019-12-03 19:48:12 +0700

    mengerjakan video #7

---
+++ application/controllers/Menu.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Menu extends CI_Controller
+{
+
+  public function index()
+  {
+    $data['title'] = 'Menu Management';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+    $data['menu'] = $this->db->get('user_menu')->result_array();
+
+    $this->form_validation->set_rules('menu', 'Menu', 'required');
+
+    if ($this->form_validation->run() == false) {
+      $this->load->view('templates/header', $data);
+      $this->load->view('templates/sidebar', $data);
+      $this->load->view('templates/topbar', $data);
+      $this->load->view('menu/index', $data);
+      $this->load->view('templates/footer');
+    } else {
+      $this->db->insert('user_menu', ['menu' => $this->input->post('menu')]);
+      $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">New menu added!</div>');
+      redirect('menu');
+    }
+  }
+
+
+  public function subMenu()
+  {
+    $data['title'] = 'Submenu Management';
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+    $this->load->model('Menu_model', 'menu');
+
+    $data['menu'] = $this->db->get('user_menu')->result_array();
+    $data['sub_menu'] = $this->menu->getSubMenu();
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $this->form_validation->set_rules('menu_id', 'Menu', 'required');
+    $this->form_validation->set_rules('url', 'URL', 'required');
+    $this->form_validation->set_rules('icon', 'Icon', 'required');
+
+    if ($this->form_validation->run() == false) {
+      $this->load->view('templates/header', $data);
+      $this->load->view('templates/sidebar', $data);
+      $this->load->view('templates/topbar', $data);
+      $this->load->view('menu/submenu', $data);
+      $this->load->view('templates/footer');
+    } else {
+      $data = [
+        'menu_id' => $this->input->post('menu_id'),
+        'title' => htmlspecialchars($this->input->post('title')),
+        'url' => htmlspecialchars($this->input->post('url')),
+        'icon' => htmlspecialchars($this->input->post('icon')),
+        'is_active' => $this->input->post('is_active')
+      ];
+
+      $this->db->insert('user_sub_menu', $data);
+      $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">New Submenu added!</div>');
+      redirect('menu/submenu');
+    }
+  }
+}
