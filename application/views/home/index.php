0b8dc4d39664b97135dccc007bcaaaa315578cc9
AdhyWiranto44 <adhywiranto68@gmail.com>
2019-12-05 21:23:34 +0700

    mengerjakan halaman home dan memindahkan autoload ke controller home

---
+++ application/views/home/index.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?= base_url('assets/vendor/bootstrap'); ?>/css/bootstrap.min.css">
+
+  <!-- My CSS -->
+  <link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
+
+  <title>soUnd</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">soUnd</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav mx-auto">
+        <a class="nav-item nav-link mr-3" href="#">Headphones</a>
+        <a class="nav-item nav-link mr-3" href="#">Earphones</a>
+        <a class="nav-item nav-link mr-3" href="#">Brands</a>
+        <a class="nav-item nav-link mr-3" href="#">About</a>
+        <a class="nav-item nav-link" href="#">Contact us</a>
+      </div>
+      <a class="btn btn-secondary mr-2" style="width: 75px;" href="<?= base_url('auth/index'); ?>" role="button">Login</a>
+      <a class="btn btn-outline-secondary" style="width: 75px;" href="<?= base_url('auth/registration'); ?>" role="button">Register</a>
+    </div>
+  </nav>
+
+  <div class="container">
+
+    <!-- Jumbotron -->
+    <div class="row mt-4">
+      <div class="col">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <a href="http://bukalapak.com">
+                <img src="<?= base_url('assets/img/carousel/1.png'); ?>" class="d-block w-100">
+              </a>
+            </div>
+            <div class="carousel-item">
+              <img src="<?= base_url('assets/img/carousel/2.png'); ?>" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+              <img src="<?= base_url('assets/img/carousel/3.png'); ?>" class="d-block w-100">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end of Jumbotron -->
+
+    <div class="row mt-4 mb-5">
+      <div class="col-sm mb-3">
+        <div class="kotak w-100 bg-dark" style="height: 150px;">
+          <div class="title pl-2 pt-2 text-light">
+            <h3>Headphones</h3>
+            <p>Enjoy your creativity</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm mb-3">
+        <div class="kotak w-100 bg-dark" style="height: 150px;">
+          <div class="title pl-2 pt-2 text-light">
+            <h3>Earphones</h3>
+            <p>Just chill</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm mb-3">
+        <div class="kotak w-100 bg-dark" style="height: 150px;">
+          <div class="title pl-2 pt-2 text-light">
+            <h3>Brands</h3>
+            <p>Find your favorite brand</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="<?= base_url('assets/vendor/jquery/'); ?>jquery.js"></script>
+  <script src="<?= base_url('assets/vendor/popper/'); ?>popper.min.js"></script>
+  <script src="<?= base_url('assets/vendor/bootstrap/'); ?>js/bootstrap.min.js"></script>
+</body>
+
+</html>
