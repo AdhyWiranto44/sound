f1e508ae06ffe3e495097a56fa7c8ea479dfd468
AdhyWiranto44 <adhywiranto68@gmail.com>
2019-12-05 12:46:45 +0700

    mengerjakan video #8

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,6 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Admin extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
     public function index()
     {
         $data['title'] = 'Dashboard';
