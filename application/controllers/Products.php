42e7240f7d0f477c9ad161e694dad4ab484454ad
RicoAristama <rico_173040052@mail.unpas.ac.id>
2019-12-08 15:36:43 +0700

    Menambah navbar di halaman headphone, earphone dan brands

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -4,19 +4,28 @@
     class Products extends CI_Controller
     {
        public function showHeadphones(){
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
         $this->load->view('templates/header_content');
+        $this->load->view('templates/navbar_content', $data);
         $this->load->view('products/headphones');
         $this->load->view('templates/footer_content');
        }
 
        public function showEarphones(){
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
         $this->load->view('templates/header_content');
+        $this->load->view('templates/navbar_content', $data);
         $this->load->view('products/earphones');
         $this->load->view('templates/footer_content');
        }
 
        public function showBrands(){
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
         $this->load->view('templates/header_content');
+        $this->load->view('templates/navbar_content', $data);
         $this->load->view('products/brands');
         $this->load->view('templates/footer_content');
        }
