780b7afd8b6d11dfaab710b11dfc5e575bcac299
AdhyWiranto44 <adhywiranto68@gmail.com>
2019-12-18 18:54:48 +0700

    menselaraskan tampilan dataEarphone dengan tampilan dataHeadphone

--- application/views/products/dataEarphones.php
+++ application/views/products/dataEarphones.php
@@ -4,15 +4,15 @@
 
     <?php echo $this->session->flashdata('message'); ?>
 
-    <button class="btn btn-warning mb-3" data-toggle="modal" data-target="#tambah_produk"><i class="fas fa-plus"></i> Add New Earphone</button>
+    <button class="btn btn-warning mb-3" data-toggle="modal" data-target="#tambah_produk"><i class="fas fa-plus"></i> Add New Headphone</button>
     <table class="table table-hover">
         <thead>
             <tr>
                 <th scope="col">#</th>
+                <th scope="col">Gambar</th>
                 <th scope="col">Menu</th>
                 <th scope="col">Merk</th>
                 <th scope="col">Harga</th>
-                <th scope="col">Gambar</th>
                 <th scope="col">Action</th>
             </tr>
         </thead>
@@ -22,14 +22,14 @@
             <?php foreach ($barang as $b) : ?>
                 <tr>
                     <th scope="row"><?= $i++; ?></th>
+                    <td>
+                        <img src="<?= base_url('assets/products/earphone/') . $b['gambar_produk']; ?>" style="height: 100px;">
+                    </td>
                     <td><?= $b['nama_produk']; ?></td>
                     <td><?= $b['merk_produk']; ?></td>
                     <td><?= $b['harga_produk']; ?></td>
                     <td>
-                        <img src="<?= base_url('assets/products/earphone/') . $b['gambar_produk']; ?>" style="height: 30px;">
-                    </td>
-                    <td>
-                        <a href="#" class="badge badge-success">Edit</a>
+                        <a href="#" class="badge badge-success" data-toggle="modal" data-target="#ubahModal">Edit</a>
                         <a href="#" class="badge badge-danger" data-toggle="modal" data-target="#hapusModal">Delete</a>
                     </td>
                 </tr>
@@ -54,18 +54,18 @@
     </div> -->
 </div>
 
-<!-- Modal -->
+<!-- Tambah Modal -->
 <div class="modal fade" id="tambah_produk" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Form Tambah Data Earphone</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Form Tambah Data Headphone</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <form action="<?php echo base_url() . 'Products/tambahEarphone'; ?>" method="post" enctype="multipart/form-data">
+                <form action="<?= base_url() . 'products/tambahheadphone'; ?>" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <label>Nama Produk</label>
                         <input type="text" name="nama_produk" class="form-control">
@@ -80,17 +80,17 @@
                     </div>
                     <div class="form-group">
                         <label>Gambar Produk</label></br>
-                        <input type="file" name="gambar_produk" class="form-control">
+                        <input type="file" name="gambar_produk" class="form-control-file">
                     </div>
             </div>
             <div class="modal-footer">
-                <button type="submit" class="btn btn-primary">Simpan</button>
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <button type="submit" class="btn btn-warning">Simpan</button>
             </div>
             </form>
         </div>
     </div>
 </div>
-
 
 <!-- Hapus Modal -->
 <div class="modal fade" id="hapusModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -107,8 +107,46 @@
             </div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a href="<?= base_url('Products/hapusEarphone/') . $b['id_headset']; ?>" class="btn btn-warning">Ya</a>
+                <a href="<?= base_url('Products/hapusHeadphone/') . $b['id_headset']; ?>" class="btn btn-warning">Ya</a>
             </div>
+        </div>
+    </div>
+</div>
+
+<!-- ubah Modal -->
+<div class="modal fade" id="ubahModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Form Ubah Data Earphone</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?php echo base_url('Products/ubahEarphone/') . $b['id_headset']; ?>" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <input type="hidden" name="id_headset" class="form-control" value="<?= $b['id_headset']; ?>">
+                        <label>Nama Produk</label>
+                        <input type="text" name="nama_produk" class="form-control" value="<?= $b['nama_produk']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label>Merk Produk</label>
+                        <input type="text" name="merk_produk" class="form-control" value="<?= $b['merk_produk']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label>Harga Produk</label>
+                        <input type="text" name="harga_produk" class="form-control" value="<?= $b['harga_produk']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label>Gambar Produk</label></br>
+                        <input type="file" name="gambar_produk" class="form-control-file" value="<?= $b['gambar_produk']; ?>">
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+            </form>
         </div>
     </div>
 </div>
